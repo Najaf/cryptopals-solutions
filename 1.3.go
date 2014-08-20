f569c410819421aa218f3e3fb38025f9762c8b52
Najaf Ali <ali.najaf@gmail.com>
2014-08-20 21:36:39 +0100

    Runs go gmt on 1.3

--- 1.3.go
+++ 1.3.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/Najaf/cryptopals-solutions/util"
-  "encoding/hex"
-  "fmt"
 )
 
 func singleByteXOR(input []byte, key byte) []byte {
-  key_bytes := make([]byte, len(input))
-  for i, _ := range key_bytes {
-    key_bytes[i] = key
-  }
+	key_bytes := make([]byte, len(input))
+	for i, _ := range key_bytes {
+		key_bytes[i] = key
+	}
 
-  return util.FixedXOR(input, key_bytes)
+	return util.FixedXOR(input, key_bytes)
 }
 
 func main() {
-  ciphertext, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	ciphertext, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
-  for i := 0; i < 256; i++ {
-    result := singleByteXOR(ciphertext, byte(i))
-    fmt.Printf("%s \n", result)
-  }
+	for i := 0; i < 256; i++ {
+		result := singleByteXOR(ciphertext, byte(i))
+		fmt.Printf("%s \n", result)
+	}
 }
