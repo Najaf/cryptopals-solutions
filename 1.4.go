54e45b9c12103f4e611ab7b6df2b28843da4d74a
Najaf Ali <ali.najaf@gmail.com>
2014-09-02 22:03:00 +0100

    Produces plaintext required for 1.4

--- 1.4.go
+++ 1.4.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "io/ioutil"
-  "strings"
+	"encoding/hex"
+	"fmt"
+	"github.com/Najaf/cryptopals-solutions/charfreq"
+	"github.com/Najaf/cryptopals-solutions/util"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-  f, _ := os.Open("challenge-data/4.txt")
-  s, _ := ioutil.ReadAll(f)
-  ciphertexts := strings.Split(string(s), "\n")
+	f, _ := os.Open("challenge-data/4.txt")
+	s, _ := ioutil.ReadAll(f)
+	ciphertexts := strings.Split(string(s), "\n")
 
-  fmt.Printf("%s\n", ciphertexts)
+	var plaintexts [][]byte
+
+	for _, encoded_ciphertext := range ciphertexts {
+		ciphertext, _ := hex.DecodeString(encoded_ciphertext)
+		for i := 0; i < 256; i++ {
+			result := util.SingleByteXOR(ciphertext, byte(i))
+			plaintexts = append(plaintexts, result)
+		}
+	}
+
+	winner, _ := charfreq.BestCharFrequencyScore(plaintexts)
+	fmt.Printf("%s\n", winner)
 }
